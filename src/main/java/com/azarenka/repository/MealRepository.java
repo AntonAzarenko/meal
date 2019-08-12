c17dd3d7917e6e511878d8ca78575ec32d0eb6e8
Anton_azarenka <anton_azarenka@epam.com>
2019-08-12 22:33:07 +0300

    addd day and meal entity

---
+++ src/main/java/com/azarenka/repository/MealRepository.java
@@ -0,0 +1,24 @@
+package com.azarenka.repository;
+
+import com.azarenka.domain.Meal;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Meal repository.
+ * <p>
+ * (c) ant-azarenko@mail.ru
+ * </p>
+ *
+ * @author Anton Azarnko
+ * Date 12 08 2019
+ */
+@Repository
+public interface MealRepository {
+
+    /**
+     * @return list of days
+     */
+    List<Meal> getAll();
+}
---
+++ src/main/java/com/azarenka/service/impl/MealServiceImpl.java
@@ -0,0 +1,23 @@
+package com.azarenka.service.impl;
+
+import com.azarenka.domain.Meal;
+import com.azarenka.service.api.MealService;
+
+import java.util.List;
+
+/**
+ * Meal service
+ * <p>
+ * (c) ant-azarenko@mail.ru
+ * </p>
+ * Date 12 08 2019
+ *
+ * @author Anton Azarnka
+ */
+public class MealServiceImpl implements MealService{
+
+    @Override
+    public List<Meal> getAll() {
+        return null;
+    }
+}
